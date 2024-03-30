634690de989d2cce7892b2ea5e3602c642dd5fbf
AlephCubed <jothebro500@gmail.com>
2024-03-29 17:16:22 -0700

    Advent of Code 1.1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::fs;
+use regex::Regex;
+
 fn main() {
-    println!("Hello, world!");
+    let contents = fs::read_to_string("src/day1.txt")
+        .expect("file present");
+
+    let mut total : i32 = 0;
+
+    for line in contents.split("\n") {
+        let regex = Regex::new(r"\D");
+        let digits = regex.unwrap().replace_all(line, "");
+        let string =
+            format!("{}{}",
+            digits.chars().nth(0).expect("expected char"),
+            digits.chars().nth(digits.len() - 1).expect("expected char"));
+        total += string.parse::<i32>().unwrap();
+    }
+
+    println!("{total}");
 }
