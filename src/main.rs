b10a9eec0bebd6ec9d35cf0046266f0ec3698263
AlephCubed <jothebro500@gmail.com>
2024-03-29 18:24:09 -0700

    AOC day 1 part 2

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,23 @@ use std::fs;
 use regex::Regex;
 
 fn main() {
+    let numbers = vec!["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
     let contents = fs::read_to_string("src/day1.txt")
         .expect("file present");
 
     let mut total : i32 = 0;
 
     for line in contents.split("\n") {
-        let regex = Regex::new(r"\D");
-        let digits = regex.unwrap().replace_all(line, "");
+
+        let mut new_line: String = line.to_string();
+        for (index, number) in numbers.iter().copied().enumerate() {
+            let result = [number, number].join(&(index + 1).to_string());
+            new_line = new_line.replace(number, &result);
+        }
+
+        let regex = Regex::new(r"\D").unwrap();
+        let digits = regex.replace_all(&new_line, "");
         let string =
             format!("{}{}",
             digits.chars().nth(0).expect("expected char"),
