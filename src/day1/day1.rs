908d99457995e52b19f4828f9f706077ba1ffca7
AlephCubed <jothebro500@gmail.com>
2024-03-29 18:42:49 -0700

    Moved code to separate file

---
+++ src/day1/day1.rs
@@ -0,0 +1,30 @@
+use std::fs;
+use regex::Regex;
+
+pub fn solve() {
+    let numbers = vec!["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
+    let contents = fs::read_to_string("src/day1/day1.txt")
+        .expect("file present");
+
+    let mut total : i32 = 0;
+
+    for line in contents.split("\n") {
+
+        let mut new_line: String = line.to_string();
+        for (index, number) in numbers.iter().copied().enumerate() {
+            let result = [number, number].join(&(index + 1).to_string());
+            new_line = new_line.replace(number, &result);
+        }
+
+        let regex = Regex::new(r"\D").unwrap();
+        let digits = regex.replace_all(&new_line, "");
+        let string =
+            format!("{}{}",
+                    digits.chars().nth(0).expect("expected char"),
+                    digits.chars().nth(digits.len() - 1).expect("expected char"));
+        total += string.parse::<i32>().unwrap();
+    }
+
+    println!("{total}");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,7 @@
-use std::fs;
-use regex::Regex;
+mod day1 {
+    pub mod day1;
+}
 
 fn main() {
-    let numbers = vec!["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-
-    let contents = fs::read_to_string("src/day1.txt")
-        .expect("file present");
-
-    let mut total : i32 = 0;
-
-    for line in contents.split("\n") {
-
-        let mut new_line: String = line.to_string();
-        for (index, number) in numbers.iter().copied().enumerate() {
-            let result = [number, number].join(&(index + 1).to_string());
-            new_line = new_line.replace(number, &result);
-        }
-
-        let regex = Regex::new(r"\D").unwrap();
-        let digits = regex.replace_all(&new_line, "");
-        let string =
-            format!("{}{}",
-            digits.chars().nth(0).expect("expected char"),
-            digits.chars().nth(digits.len() - 1).expect("expected char"));
-        total += string.parse::<i32>().unwrap();
-    }
-
-    println!("{total}");
+    day1::day1::solve();
 }
