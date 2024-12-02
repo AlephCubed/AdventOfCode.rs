bf871415c53d0b0c19348269b1f69a0a324d4ceb
AlephCubed <jothebro500@gmail.com>
2024-12-02 15:59:42 -0800

    Test cases galore.

--- src/year24/day2.rs
+++ src/year24/day2.rs
@@ -48,167 +48,175 @@ pub fn solve_b() {
     let lines = io::BufReader::new(file).lines();
     let mut count = 0;
 
-    'outer: for line in lines.flatten() {
+    for line in lines.flatten() {
         println!("\nLine: {line}");
-        let mut nums = line.split(' ').map(|s| {
+        let nums: Vec<i8> = line.split(' ').map(|s| {
             s.parse::<i8>().unwrap()
-        });
+        }).collect();
 
-        let mut a = nums.next().unwrap();
-        let mut state = State::Equal;
-        let mut used = false;
-        let mut ignored = false;
-        
-        for (b, c) in nums.clone().zip(nums.clone().skip(1)) {
-            let mut ig = false;
-            println!("{a}, {b}, {c}, state: {:?}, used: {used}.", state);
-            let state_a = get_state(a, b);
-            let state_b = get_state(b, c);
-            let state_c = get_state(a, c);
-            println!("a: {:?}, b: {:?}, c: {:?}.", state_a, state_b, state_c);
-            
-            if state_a == state_b {
-                if !state_a.is_err() { // All good.
-                    println!("Good.");
-                    a = b;
-                    state = state_a;
-                    
-                // Three errors, unable to remove middle value (b). Unrecoverable.
-                } else if state_c.is_err() || used { 
-                    println!("Three Errors or used.\n");
-                    continue 'outer; 
-                    
-                } else {
-                    // Skip middle value (b).
-                    println!("Skipping b: {b}.");
-                    used = true;
-                    state = state_c;
-                }
-            } else { // Try to recover.
-                // We already used, or they are mismatched errors, we are done.
-                if state_a.is_err() && state_b.is_err() {
-                    println!("Mismatched errors.");
-                    continue 'outer;
-                }
-
-                
-                if state_a.is_err() {
-                    // If first pass and error is (a), remove (a).
-                    if state.is_err() {
-                        if used { 
-                            println!("Already used (1).\n"); 
-                            continue 'outer; 
-                        }
-                        
-                        println!("Skipping a: {a}.");
-                        used = true;
-                        a = b;
-                        state = state_b;
-                    } else if state_c.is_err() {
-                        println!("(a) and (c) are errors.");
-                        continue 'outer;
-                    } else {
-                        if used {
-                            println!("Already used (1.5).\n");
-                            continue 'outer;
-                        }
-
-                        println!("Ignoring - (b) error.");
-                        ig = true;
-                        a = b;
-                        state = state_a;
-                    }
-                } else if state_b.is_err() {
-                    if !state_c.is_err() { // Try removing (b).
-                        if used {
-                            println!("Already used (2).\n");
-                            continue 'outer;
-                        }
-
-                        println!("Skipping b1.5: {b}.");
-                        used = true;
-                        state = state_c;
-                    } else {
-                        // (a) is ok, (b) is err, (c) is err.
-                        println!("Ignoring - (b) (c) err.");
-                        ig = true;
-                        a = b;
-                        state = state_a;
-                    }
-
-                // Neither are errors, but don't match.
-                } else if !state_c.is_err() {
-                    if state_c == state || state.is_err() {
-                        if used {
-                            println!("Already used (3).\n");
-                            continue 'outer;
-                        }
-                        
-                        println!("Skipping b2: {b}.");
-                        used = true;
-                        state = state_c;
-                    } else if state_a == state {
-                        if used {
-                            println!("Already used (4).\n");
-                            continue 'outer;
-                        }
-                        
-                        println!("Ignoring - Deal with peak next iteration.");
-                        ig = true;
-                        a = b;
-                    } else {
-                        println!("Unable to remove peak.");
-                        continue 'outer;
-                    }
-                } else {// Ignore. Catch on the next iteration.
-                    if state.is_err() {// First iteration, can remove (a).
-                        if used {
-                            println!("Already used (5).\n");
-                            continue 'outer;
-                        }
-                        
-                        println!("Skipping a2: {a}.");
-                        used = true;
-                        a = b;
-                    } else {
-                        if used {
-                            println!("Already used (6).\n");
-                            continue 'outer;
-                        }
-                        
-                        println!("Ignoring - Can't skip (a) or (b).");
-                        ig = true;
-                        a = b;
-                        state = state_a;
-                    }
-                }
-            }
-            
-            if ig && ignored {
-                println!("Double ignored.");
-                continue 'outer;
-            }
-            ignored = ig;
+        if check_line(nums) {
+            count += 1;
         }
-        
-        if ignored && used {
-            let mut rev = nums.rev();
-            let a = rev.next().unwrap();
-            let b = rev.next().unwrap();
-            let stat_a = get_state(a, b);
-            
-            if state != stat_a {
-                println!("Ignored final with use, err.");
-                continue 'outer;
-            }
-            println!("Ignored final with use, ok.");
-        }
-
-        println!("Passed!");
-        count += 1;
     }
 
     println!("Result {count}");
+}
+
+fn check_line(nums: Vec<i8>) -> bool {
+    let mut nums = nums.iter();
+    
+    let mut a = nums.next().unwrap();
+    let mut state = State::Equal;
+    let mut used = false;
+    let mut ignored = false;
+
+    for (b, c) in nums.clone().zip(nums.clone().skip(1)) {
+        let mut ig = false;
+        println!("{a}, {b}, {c}, state: {:?}, used: {used}.", state);
+        let state_a = get_state(a, b);
+        let state_b = get_state(b, c);
+        let state_c = get_state(a, c);
+        println!("a: {:?}, b: {:?}, c: {:?}.", state_a, state_b, state_c);
+
+        if state_a == state_b {
+            if !state_a.is_err() { // All good.
+                println!("Good.");
+                a = b;
+                state = state_a;
+
+            // Three errors, unable to remove middle value (b). Unrecoverable.
+            } else if state_c.is_err() || used {
+                println!("Three Errors or used.\n");
+                return false;
+
+            } else {
+                // Skip middle value (b).
+                println!("Skipping b: {b}.");
+                used = true;
+                state = state_c;
+            }
+        } else { // Try to recover.
+            // We already used, or they are mismatched errors, we are done.
+            if state_a.is_err() && state_b.is_err() {
+                println!("Mismatched errors.");
+                return false;
+            }
+
+
+            if state_a.is_err() {
+                // If first pass and error is (a), remove (a).
+                if state.is_err() {
+                    if used {
+                        println!("Already used (1).\n");
+                        return false;
+                    }
+
+                    println!("Skipping a: {a}.");
+                    used = true;
+                    a = b;
+                    state = state_b;
+                } else if state_c.is_err() {
+                    println!("(a) and (c) are errors.");
+                    return false;
+                } else {
+                    if used {
+                        println!("Already used (1.5).\n");
+                        return false;
+                    }
+
+                    println!("Ignoring - (b) error.");
+                    ig = true;
+                    a = b;
+                    state = state_a;
+                }
+            } else if state_b.is_err() {
+                if !state_c.is_err() { // Try removing (b).
+                    if used {
+                        println!("Already used (2).\n");
+                        return false;
+                    }
+
+                    println!("Skipping b1.5: {b}.");
+                    used = true;
+                    state = state_c;
+                } else {
+                    // (a) is ok, (b) is err, (c) is err.
+                    println!("Ignoring - (b) (c) err.");
+                    ig = true;
+                    a = b;
+                    state = state_a;
+                }
+
+            // Neither are errors, but don't match.
+            } else if !state_c.is_err() {
+                if state_c == state || state.is_err() {
+                    if used {
+                        println!("Already used (3).\n");
+                        return false;
+                    }
+
+                    println!("Skipping b2: {b}.");
+                    used = true;
+                    state = state_c;
+                } else if state_a == state {
+                    if used {
+                        println!("Already used (4).\n");
+                        return false;
+                    }
+
+                    println!("Ignoring - Deal with peak next iteration.");
+                    ig = true;
+                    a = b;
+                } else {
+                    println!("Unable to remove peak.");
+                    return false;
+                }
+            } else {// Ignore. Catch on the next iteration.
+                if state.is_err() {// First iteration, can remove (a).
+                    if used {
+                        println!("Already used (5).\n");
+                        return false;
+                    }
+
+                    println!("Skipping a2: {a}.");
+                    used = true;
+                    a = b;
+                } else {
+                    if used {
+                        println!("Already used (6).\n");
+                        return false;
+                    }
+
+                    println!("Ignoring - Can't skip (a) or (b).");
+                    ig = true;
+                    a = b;
+                    state = state_a;
+                }
+            }
+        }
+
+        if ig && ignored {
+            println!("Double ignored.");
+            return false;
+        }
+        ignored = ig;
+    }
+
+    if ignored && used {
+        let mut rev = nums.rev();
+        let a = rev.next().unwrap();
+        let b = rev.next().unwrap();
+        let stat_a = get_state(a, b);
+
+        if state != stat_a {
+            println!("Ignored final with use, err.");
+            return false;
+        }
+        println!("Ignored final with use, ok.");
+    }
+
+    println!("Passed!");
+    true
 }
 
 #[derive(Eq, PartialEq, Debug)]
@@ -228,7 +236,7 @@ impl State {
     }
 }
 
-fn get_state(a: i8, b: i8) -> State {
+fn get_state(a: &i8, b: &i8) -> State {
     let dif = a - b;
     
     if dif == 0 {
@@ -239,5 +247,160 @@ fn get_state(a: i8, b: i8) -> State {
         State::Increasing
     } else {
         State::OutOfBounds
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn test_simple_one_offset() {
+        let vec = vec![1, 2, 3, 4];
+        assert_eq!(check_line(vec), true);
+    }
+
+    #[test]
+    fn test_simple_two_offset() {
+        let vec = vec![1, 3, 5, 7];
+        assert_eq!(check_line(vec), true);
+    }
+
+    #[test]
+    fn test_simple_three_offset() {
+        let vec = vec![1, 4, 7, 10];
+        assert_eq!(check_line(vec), true);
+    }
+
+    #[test]
+    fn test_simple_down() {
+        let vec = vec![4, 3, 2, 1];
+        assert_eq!(check_line(vec), true);
+    }
+    
+    #[test]
+    fn test_err_jump() {
+        let vec = vec![1, 3, 7, 8];
+        assert_eq!(check_line(vec), false);
+    }
+
+    #[test]
+    fn test_err_jump_down() {
+        let vec = vec![10, 7, 3, 1];
+        assert_eq!(check_line(vec), false);
+    }
+
+    #[test]
+    fn test_err_dup_save() {
+        let vec = vec![1, 2, 2, 3];
+        assert_eq!(check_line(vec), true);
+    }
+
+    #[test]
+    fn test_err_trip() {
+        let vec = vec![1, 2, 2, 2];
+        assert_eq!(check_line(vec), false);
+    }
+    
+    #[test]
+    fn test_err_peak_save() {
+        let vec = vec![1, 2, 4, 3];
+        assert_eq!(check_line(vec), true);
+    }
+
+    #[test]
+    fn test_err_peak_dup() {
+        let vec = vec![1, 2, 3, 2, 3];
+        assert_eq!(check_line(vec), false);
+    }
+
+    #[test]
+    fn test_err_peak_swap() {
+        let vec = vec![1, 2, 3, 1, 3];
+        assert_eq!(check_line(vec), false);
+    }
+
+    #[test]
+    fn test_err_jump_save_end() {
+        let vec = vec![1, 3, 6, 10];
+        assert_eq!(check_line(vec), true);
+    }
+
+    #[test]
+    fn test_err_jump_save_begin() {
+        let vec = vec![1, 4, 7, 10];
+        assert_eq!(check_line(vec), true);
+    }
+    // ---===---
+    
+    #[test]
+    fn test_case_dup_plus_peak() {
+        let vec = vec![8, 9, 11, 11, 13, 10];
+        assert_eq!(check_line(vec), false);
+    }
+    
+    #[test]
+    fn test_case_dup_plus_end_jump() {
+        let vec = vec![25, 27, 28, 28, 31, 35];
+        assert_eq!(check_line(vec), false);
+    }
+    
+    #[test]
+    fn test_case_dup_plus_2_jump() {
+        let vec = vec![71, 73, 73, 76, 82];
+        assert_eq!(check_line(vec), false);
+    }
+    
+    #[test]
+    fn test_case_2_end_jump() {
+        let vec = vec![3, 5, 6, 8, 11, 12, 16, 19];
+        assert_eq!(check_line(vec), false);
+    }
+    #[test]
+    fn test_case_jump_plus_dup_peak() {
+        let vec = vec![59, 62, 66, 69, 71, 69];
+        assert_eq!(check_line(vec), false);
+    }
+    
+    #[test]
+    fn test_case_jump_plus_dup() {
+        let vec = vec![19, 20, 24, 25, 26, 26];
+        assert_eq!(check_line(vec), false);
+    }
+    
+    #[test]
+    fn test_case_2_jump() {
+        let vec = vec![1, 2, 6, 8, 12];
+        assert_eq!(check_line(vec), false);
+    }
+    
+    #[test]
+    fn test_case_jump_plus_end_jump() {
+        let vec = vec![61, 62, 63, 66, 68, 69, 73, 78];
+        assert_eq!(check_line(vec), false);
+    }
+    
+    #[test]
+    fn test_case_large_jumps() {
+        let vec = vec![43, 46, 48, 55, 58, 59, 60, 63];
+        assert_eq!(check_line(vec), false);
+    }
+    
+    #[test]
+    fn test_case_large_jump_plus_peak() {
+        let vec = vec![35, 38, 40, 45, 46, 47, 44];
+        assert_eq!(check_line(vec), false);
+    }
+    
+    #[test]
+    fn test_case_end_jump_plus_dup() {
+        let vec = vec![84, 85, 86, 87, 94, 94];
+        assert_eq!(check_line(vec), false);
+    }
+    
+    #[test]
+    fn test_case_large_jump2() {
+        let vec = vec![80, 83, 90, 91, 93, 95, 99];
+        assert_eq!(check_line(vec), false);
     }
 }
