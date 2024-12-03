25429256d81ff9fd898b8c8104b5f0948e0a62ee
AlephCubed <jothebro500@gmail.com>
2024-12-02 22:18:32 -0800

    D3b

--- src/utils.rs
+++ src/utils.rs
@@ -2,6 +2,7 @@ use std::fmt::Debug;
 use std::str::FromStr;
 use regex::Regex;
 
+#[allow(dead_code)]
 #[deprecated]
 pub fn get_num<T: FromStr>(input: &str) -> T 
     where <T as FromStr>::Err: Debug 
--- src/year24/day3.rs
+++ src/year24/day3.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io;
+use std::{fs, io};
 use std::io::BufRead;
 use regex::Regex;
 
@@ -26,5 +26,24 @@ pub fn solve_a() {
 }
 
 pub fn solve_b() {
+    let contents = fs::read_to_string("src/year24/day3.txt")
+        .expect("file present");
+    let regex = Regex::new(r"mul\(\d?\d?\d?,\d?\d?\d?\)").expect("valid regex");
+    let strip_regex = Regex::new(r"(?s)don't\(\).*?do\(\)").expect("valid regex");
+
+    let mut total = 0;
+
+    let string = strip_regex.replace_all(&contents, "");
     
+    for mul in regex.find_iter(&*string) {
+        let s = mul.as_str();
+        let s = s.strip_prefix("mul(").unwrap();
+        let s = s.strip_suffix(')').unwrap();
+        let num_strings = s.split(',');
+        let nums = num_strings.map(|s| s.parse::<u32>().unwrap());
+
+        total += nums.product::<u32>();
+    }
+
+    println!("Result: {total}");
 }
